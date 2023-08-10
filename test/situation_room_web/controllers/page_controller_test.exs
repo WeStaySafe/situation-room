ea08db59de3057b7367d5ab2194a32abf97a8e15
Charif Mews <charif@westaysafe.com>
2023-08-10 12:55:32 +0200

    Story/Landing page, still need to replace app placeholder image

--- test/situation_room_web/controllers/page_controller_test.exs
+++ test/situation_room_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule SituationRoomWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "Give your community the perfect tool to monitor"
   end
 end
