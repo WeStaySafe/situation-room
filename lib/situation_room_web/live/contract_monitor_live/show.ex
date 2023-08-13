618af56942a6b1ac221bb66ada2c9d5dda7e09b3
Charif Mews <charif@westaysafe.com>
2023-08-13 15:30:35 +0200

    Live logs dashboard

--- lib/situation_room_web/live/contract_monitor_live/show.ex
+++ lib/situation_room_web/live/contract_monitor_live/show.ex
@@ -5,6 +5,8 @@ defmodule SituationRoomWeb.ContractMonitorLive.Show do
 
   @impl true
   def mount(_params, _session, socket) do
+    if connected?(socket), do: :timer.send_interval(2000, self(), :refresh)
+
     {:ok, socket |> assign(:current_user, nil)}
   end
 
@@ -14,9 +16,17 @@ defmodule SituationRoomWeb.ContractMonitorLive.Show do
      socket
      |> assign(:page_title, page_title(socket.assigns.live_action))
      |> assign(:chain_id, chain_id)
+     |> assign(:contract_monitor_id, id)
      |> assign(:contract_monitor, Chains.get_contract_monitor_with_logs!(id))}
   end
 
   defp page_title(:show), do: "Show Contract monitor"
   defp page_title(:edit), do: "Edit Contract monitor"
+
+  @impl true
+  def handle_info(:refresh, socket) do
+    {:noreply,
+     socket
+     |> assign(:contract_monitor, Chains.get_contract_monitor_with_logs!(socket.assigns.contract_monitor_id))}
+  end
 end
